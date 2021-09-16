0fd6087836dbe87c762817eac3896beb8262bd0f
Nathan <maximenathan@hotmail.fr>
2021-09-16 05:30:43 +0200

    cpp08: add ex02

---
+++ cpp08/ex02/includes/MutantStack.hpp
@@ -0,0 +1,36 @@
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <iostream>
+# include <string>
+# include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+
+	public:
+
+		MutantStack(): std::stack<T>()
+		{}
+		MutantStack( MutantStack const & src ) :std::stack<T>(src)
+		{} 
+		~MutantStack()
+		{}
+
+		typedef typename std::stack<T>::container_type::iterator iterator;
+
+		iterator	begin( void )
+		{
+			return this->c.begin();
+		}
+		iterator	end( void )
+		{
+			return this->c.end();
+		}
+
+	private:
+
+};
+
+#endif /* ***************************************************** MUTANTSTACK_H */
